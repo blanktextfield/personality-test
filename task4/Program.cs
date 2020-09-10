033a23408b3a48c8328b329c4f7c633855c1f82e
Opilane TTHK <opilane@tthk.ee>
2020-09-10 12:23:57 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+            int Number;
+            Console.WriteLine("what is your number?");
+            UserInput = Console.ReadLine();
+            Number = Int32.Parse(UserInput);
+            if (Number < 0)
+            {
+                Console.WriteLine($"the provided number is negative");
+            }
+            else if (Number > 0)
+            {
+                Console.WriteLine($"the provided number is positive");
+            } else
+            {
+                Console.WriteLine("The number is 0");
+            }
+
+        }
+    }
+}
